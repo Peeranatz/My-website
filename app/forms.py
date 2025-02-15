0a779052185e27469d5af14e75b68122e4f29e9b
Peeranat Chunhok <Peeranat280648@gmail.com>
2025-02-15 21:51:46 +0700

    ทำให้สามารถใช้งานระบบ register และ ระบบ login ได้

--- app/forms.py
+++ app/forms.py
@@ -1,49 +1,24 @@
-# from flask_wtf import FlaskForm
-# from wtforms import StringField, PasswordField, SubmitField, TextAreaField
-# from wtforms.validators import DataRequired, Length
-
-# class RegistrationForm(FlaskForm):
-#     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=50)])
-#     password = PasswordField('Password', validators=[DataRequired()])
-#     submit = SubmitField('Sign Up')
-
-# class LoginForm(FlaskForm):
-#     username = StringField('Username', validators=[DataRequired()])
-#     password = PasswordField('Password', validators=[DataRequired()])
-#     submit = SubmitField('Login')
-
-# class SongForm(FlaskForm):
-#     title = StringField('Title', validators=[DataRequired()])
-#     artist = StringField('Artist', validators=[DataRequired()])
-#     genre = StringField('Genre', validators=[DataRequired()])
-#     submit = SubmitField('Add Song')
-
-# class PlaylistForm(FlaskForm):
-#     name = StringField('Playlist Name', validators=[DataRequired()])
-#     submit = SubmitField('Create Playlist')
-
-# ---------------------------------------------------------------------------------------------------------
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, EqualTo
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Length
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=20)])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Register')
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=50)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign Up')
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
 
-class SongForm(FlaskForm):  # ✅ เพิ่มคลาส SongForm
+class SongForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     artist = StringField('Artist', validators=[DataRequired()])
     genre = StringField('Genre', validators=[DataRequired()])
     submit = SubmitField('Add Song')
 
-class PlaylistForm(FlaskForm):  # ✅ เพิ่มคลาส PlaylistForm
+class PlaylistForm(FlaskForm):
     name = StringField('Playlist Name', validators=[DataRequired()])
     submit = SubmitField('Create Playlist')
+
